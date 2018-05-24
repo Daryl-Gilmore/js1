c3dde5ec5ed04b887efd6036f97e8786b2e4ce33
unknown <dgilmore1966@aol.com>
2018-05-23 20:47:51 -0700

    last changes to day one

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,6 @@ function(yes) {
 
 console.log(myName);
 console.log("MY GOAL IS TO LEARN AS MUCH AS POSSIBLE ABOUT BUILDING WEBSITES, CYBER SECURITY AND ALL AROUND COMPUTTER KNOWLEDGE. MY FAVORITE HOBBY, THAT'S HARD TO SAY FISHING/PISMO DUNES/SWIMMING I HAVE A FEW." )
+var name = "Daryl Gilmore";
+let birthday = new Date('12.17.66');
+const goal = 'To learn js1!!';
