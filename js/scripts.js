5dfd555c43ee54e646d0672d12bfb4cfda4f9ddd
unknown <dgilmore1966@aol.com>
2018-05-25 12:21:12 -0700

    finish day-2

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,27 @@ console.log("MY GOAL IS TO LEARN AS MUCH AS POSSIBLE ABOUT BUILDING WEBSITES, CY
 var name = "Daryl Gilmore";
 let birthday = new Date('12.17.66');
 const goal = 'To learn js1!!';
+let favmovie = "Top Gun";
+let favFood ="Tacos";
+const year = new Date(1986);
+const ingredients = ["hamburger,tomato,"];
+alert(favFood + ingredients);
+confirm(favmovie + year);
+
+//ask for user first name
+var firstName = prompt("What is your first name");
+
+//ask for user last name
+var lastName = prompt("What is your last name");
+
+//Logs the user's first name to the console
+console.log(firstName);
+
+//Alerts the user's last name
+alert(lastName);
+
+//Confirms the user's birthday
+let result = confirm(birthday);
+
+//alerts the result
+alert(result);
