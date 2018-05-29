010541b2cac54772ceba32b59672ef489977b1ce
unknown <dgilmore1966@aol.com>
2018-05-28 18:01:44 -0700

    day-2

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var myName = "Daryl Gilmore";
+/*
+function(yes) {
+  alert(yes);
+}();
+*/
+
+console.log(myName);
+console.log("MY GOAL IS TO LEARN AS MUCH AS POSSIBLE ABOUT BUILDING WEBSITES, CYBER SECURITY AND ALL AROUND COMPUTTER KNOWLEDGE. MY FAVORITE HOBBY, THAT'S HARD TO SAY FISHING/PISMO DUNES/SWIMMING I HAVE A FEW." )
+var name = "Daryl Gilmore";
+let birthday = new Date('12.17.66');
+const goal = 'To learn js1!!';
+let favmovie = "Top Gun";
+let favFood ="Tacos";
+const year = new Date(1986);
+const ingredients = ["hamburger,tomato,"];
+alert(favFood + ingredients);
+confirm(favmovie + year);
+
+//ask for user first name
+var firstName = prompt("What is your first name");
+
+//ask for user last name
+var lastName = prompt("What is your last name");
+
+//Logs the user's first name to the console
+console.log(firstName);
+
+//Alerts the user's last name
+alert(lastName);
+
+//Confirms the user's birthday
+let result = prompt("what's your birth day","Decenber 17-1966");
+confirm(new Date(result));
+
+//alerts the result
+alert(result);
